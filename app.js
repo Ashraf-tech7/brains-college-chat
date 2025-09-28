6eed6d953b1e68b5db0adeef8be81c5d4d218f5c
Ashraf-tech7 <ashrafaliyu96@gmail.com>
2025-09-28 10:35:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-
+function loadPage(page) {
+  fetch(page)
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById("content").innerHTML = html;
+    })
+    .catch(err => {
+      document.getElementById("content").innerHTML = "<p>Page not found.</p>";
+    });
+}
